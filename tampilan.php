507f5d26af3718cd7366deba0bf3451ea73a8d58
bastiannn <bastiansitanggang09102005@gmail.com>
2025-09-04 10:00:25 +0700

    Create tampilan.php

---
+++ tampilan.php
@@ -0,0 +1,23 @@
+<?php include 'partials/header.php'; ?>
+
+<div class="monitoring-content">
+    <h2>PORSEA</h2>
+    <h1>MONITORING WEATHER</h1>
+    <div class="map-monitoring">
+        MAP PETA PORSEA
+    </div>
+    <div class="data-grid">
+        <div class="data-card">Suhu</div>
+        <div class="data-card">Kelembapan</div>
+        <div class="data-card">Curah Hujan</div>
+        <div class="data-card">Jumlah Petir</div>
+        <div class="data-card">Kecepatan Angin</div>
+        <div class="data-card">Radiasi</div>
+        <div class="data-card">Cahaya</div>
+    </div>
+</div>
+
+<?php // Di sini bisa ditambahkan footer jika ada ?>
+</main>
+</body>
+</html>
