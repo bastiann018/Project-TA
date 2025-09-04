1d3278a451670c3459ed2d8784727e1ec92bf4f1
bastiannn <bastiansitanggang09102005@gmail.com>
2025-09-04 10:00:29 +0700

    Create kontak.php

---
+++ kontak.php
@@ -0,0 +1,15 @@
+<?php include 'partials/header.php'; ?>
+
+<div class="profile-page">
+    <div class="profile-card">
+        <h3>Profil Anggota Project</h3>
+        </div>
+    <div class="profile-card">
+        <h3>Profil Dosen Pembimbing</h3>
+        </div>
+</div>
+
+<?php // Di sini bisa ditambahkan footer jika ada ?>
+</main>
+</body>
+</html>
