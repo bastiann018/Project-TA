32891b2d4b02f9b757762d789c5ec76810c8f797
bastiannn <bastiansitanggang09102005@gmail.com>
2025-10-09 13:40:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,23 @@
 <div class="main-layout">
     <section class="top-section">
         <div class="project-intro">
-            <h1>JUDUL PROJECT</h1>
+            <h1>SELF SUFFICIENT IoT WEATHER MONITORING FOR WEATHER
+                DATA AS A SERVICE</h1>
             <hr class="title-divider">
-            <p>Penjelasan tentang website</p>
+            <p>Dashboard ini adalah pusat kontrol dan antarmuka utama dari sistem pemantauan cuaca mandiri (Self-Sufficient IoT). 
+                Fungsinya adalah menyediakan Layanan Data Cuaca (Weather Data as a Service).</p>
         </div>
         <div class="map-container">
             <div class="map-box">
-                PETA KAB. TOBA
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15962.336113840706!2d99.07008719999999!3d2.3855581999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e1288f3427189%3A0x6d5e168a278917e!2sKabupaten%20Toba%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1699090000000!5m2!1sid!2sid"
+                    width="100%" 
+                    height="100%" 
+                    style="border:0;" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer">
+                </iframe>
             </div>
         </div>
     </section>
